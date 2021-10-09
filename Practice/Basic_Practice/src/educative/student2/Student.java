51bf4cf1cc8e21b5b84acaaa7c40bdb29e68dbc9
Dutta-SD <duttasandip11100@gmail.com>
2021-10-09 15:49:50 +0530

    student2 getter setter

---
+++ Practice/Basic_Practice/src/educative/student2/Student.java
@@ -0,0 +1,22 @@
+package student2;
+
+public class Student {
+    private String name;
+    private String rollNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }    
+}
