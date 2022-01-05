8080ad23ada3b5911eadb6c072cfc565bacc5c92
Dutta-SD <duttasandip11100@gmail.com>
2022-01-05 23:44:18 +0530

    Added Simple Business Logic

---
+++ spring-learn/src/main/java/com/sandiplearns/springlearn/sample/enterprise/flow/Controller.java
@@ -0,0 +1,36 @@
+package com.sandiplearns.springlearn.sample.enterprise.flow;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Business Logic
+//
+@Component
+class BusinessService {
+    public long calculateSum() {
+        return 150;
+    }
+
+}
+
+// return in right format
+// Controller is a specialised component
+@RestController
+public class Controller {
+
+    @Autowired
+    private BusinessService businessService;
+
+    @GetMapping("/sum")
+    public long displaySum() {
+        // use business service
+        return businessService.calculateSum();
+    }
+
+}
+
+class DataService {
+
+}
