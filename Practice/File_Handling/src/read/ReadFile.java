d74784d4aec9ec341aeb51fb2ee29ef1549ffeb6
Dutta-SD <duttasandip11100@gmail.com>
2022-01-05 11:47:42 +0530

    Added code for basic file handling

---
+++ Practice/File_Handling/src/read/ReadFile.java
@@ -0,0 +1,26 @@
+package read;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File myObjFile = new File("File_Handling/src/read/sample.txt");
+            Scanner myReader = new Scanner(myObjFile);
+
+            System.out.println(myObjFile.exists());
+            System.out.println(myObjFile.lastModified());
+            System.out.println(myObjFile.canRead());
+
+            while(myReader.hasNextLine()){
+                System.out.println(myReader.nextLine());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Exception thrown");
+        }
+    }
+
+}
