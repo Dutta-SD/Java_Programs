2fbd08c3d8359c6d52904e908c1a9d9058131ea9
Dutta-SD <duttasandip11100@gmail.com>
2022-01-22 02:15:32 +0530

    New Spring-Core Project

---
+++ sandip/src/main/java/com/learnspring/sandip/App.java
@@ -0,0 +1,23 @@
+package com.learnspring.sandip;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/*
+ * Spring initial container
+ *  Main class, IOC container
+ *  Defined using Spring
+ *  
+ *  IOC container keeps ready made objects.
+ *  Once needed, we can inject as per need.
+ */
+
+public class App {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+		Student s = (Student) context.getBean("student1");
+
+		System.out.println(s);
+
+	}
+}
