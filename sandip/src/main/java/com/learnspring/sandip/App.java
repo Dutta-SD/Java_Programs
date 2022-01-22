a014e38d4f3bf4b04eebf3473ba3285e57ee1c90
Dutta-SD <duttasandip11100@gmail.com>
2022-01-22 15:37:54 +0530

    Added bean methods-xml, interface, annot

--- sandip/src/main/java/com/learnspring/sandip/App.java
+++ sandip/src/main/java/com/learnspring/sandip/App.java
@@ -11,13 +11,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *  Once needed, we can inject as per need.
  */
 
-public class App {
-	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-
-		Student s = (Student) context.getBean("student1");
-
-		System.out.println(s);
-
-	}
-}
+//public class App {
+//	public static void main(String[] args) {
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+//
+//		Student s = (Student) context.getBean("student1");
+//
+//		System.out.println(s);
+//
+//	}
+//}
