470090e654ee43d6eb37b2d017ea46d7068d1ae4
Dutta-SD <duttasandip11100@gmail.com>
2022-03-11 22:24:01 +0530

    Started Spring Boot

---
+++ firstboot/src/main/java/com/sandip/firstboot/MainController.java
@@ -0,0 +1,16 @@
+package com.sandip.firstboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("inside Home");
+		return "home";
+
+	}
+
+}
