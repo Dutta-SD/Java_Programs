22bc4f47cfdf722bda6e01ab0d94a1029e67dcd4
Dutta-SD <duttasandip11100@gmail.com>
2022-04-27 22:47:20 +0530

    Modified POM for dependencies

--- blog_app_apis/src/test/java/com/example/demo/BlogAppApisApplicationTests.java
+++ blog_app_apis/src/test/java/com/example/demo/BlogAppApisApplicationTests.java
@@ -1,4 +1,4 @@
-package edu.sandip.blog_app_apis;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BlogAppApisApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
