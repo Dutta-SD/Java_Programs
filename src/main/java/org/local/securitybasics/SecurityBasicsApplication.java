35862cb9b9556ad1adf6e01ef8042d5fcfa1b8d0
Dutta-SD <duttasandip11100@gmail.com>
2023-07-26 18:49:40 +0530

    Basic Security Added

--- src/main/java/org/local/securitybasics/SecurityBasicsApplication.java
+++ src/main/java/org/local/securitybasics/SecurityBasicsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecurityBasicsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityBasicsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityBasicsApplication.class, args);
+    }
 
 }
---
+++ src/main/java/org/local/securitybasics/controller/SampleController.java
@@ -0,0 +1,17 @@
+package org.local.securitybasics.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RestController
+public class SampleController {
+
+    @GetMapping(value = "/welcome")
+    public String sampleApi() {
+        return "Welcome to this page";
+    }
+}
