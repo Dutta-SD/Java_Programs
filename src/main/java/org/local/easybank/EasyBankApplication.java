810df1c88e9ca8bde2a7775456dba90e98b7c2ee
Dutta-SD <duttasandip11100@gmail.com>
2023-07-27 12:09:09 +0530

    Added new style security config

--- src/main/java/org/local/easybank/EasyBankApplication.java
+++ src/main/java/org/local/easybank/EasyBankApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityBasicsApplication {
+public class EasyBankApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecurityBasicsApplication.class, args);
+        SpringApplication.run(EasyBankApplication.class, args);
     }
 
 }
--- src/main/java/org/local/easybank/controller/BalanceController.java
+++ src/main/java/org/local/easybank/controller/BalanceController.java
@@ -5,12 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RequiredArgsConstructor
 @RestController
-public class AccountController {
-    @GetMapping(value = "/account")
-    public String getAccountDetails() {
-        return "Here are your account details";
+@RequiredArgsConstructor
+public class BalanceController {
+
+    @GetMapping("/balance")
+    public String getBalanceDetails() {
+        return "This is balance details from DB";
     }
 }
